d32d0a2c56a0170f56a0709497b9841e8aa232f6
ramesh-vyboina <ramesh.dev408@gmail.com>
2025-07-08 20:45:17 +0530

    Update subnet configuration for Azure Key Vault private endpoint

--- keyvault/private_endpoint.tf
+++ keyvault/private_endpoint.tf
@@ -2,7 +2,7 @@ resource "azurerm_private_endpoint" "kv_pe" {
   name                = "kv-testing-private-endpoint"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.aks_subnet.id
+  subnet_id           = azurerm_subnet.private_endpoint_subnet.id
 
   private_service_connection {
     name                           = "kv-testing-privatesc"
