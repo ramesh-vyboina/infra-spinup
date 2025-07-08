ec1920379133fab6c99007b50e1b107609ea0ecd
ramesh-vyboina <ramesh.dev408@gmail.com>
2025-07-08 20:40:05 +0530

    Update subnet name for private endpoint configuration

--- keyvault/subnet.tf
+++ keyvault/subnet.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_subnet" "private_endpoint_subnet" {
-  name                 = "private-endpoint-subnet"
+  name                 = "kv-private-endpoint-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.4.0/24"]
