2f73dedd122480c67e4f411cdb4ea91b04b001f9
ramesh-vyboina <ramesh.dev408@gmail.com>
2025-07-08 20:39:51 +0530

    Replace AKS subnet configuration with private endpoint subnet definition

--- keyvault/subnet.tf
+++ keyvault/subnet.tf
@@ -1,17 +1,9 @@
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-sub-eus"
+
+
+resource "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "private-endpoint-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.3.0/24"]
-
-  delegation {
-    name = "aks-delegation"
-
-    service_delegation {
-      name = "Microsoft.ContainerService/managedClusters"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/action",
-      ]
-    }
-  }
+  address_prefixes     = ["10.10.4.0/24"]
+  # No delegation for private endpoint subnet
 }
